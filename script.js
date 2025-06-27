d6a10d8fb61acbb87ca6424dadb9e0acf959208e
luisgustavo2012 <lgroquedesiderio@gmail.com>
2025-06-27 20:36:15 -0300

    ..

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function iniciarJogo(){
+    let num1 =parseInt(prompt("Digite o primeiro número inteiro:"));
+    let num2 = parseInt(prompt("Digite o segundo número inteiro:"));
 
+    let resultado = num1 + num2;
+    alert("A Soma dos números é: " = resultado);
+}
